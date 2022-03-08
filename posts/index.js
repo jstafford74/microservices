4afca6909065bd9d0cb1925fe51af36d81841e6b
jeremiah.b.stafford <jeremiah.stafford@nuborders.com>
2022-03-08 07:36:54 -0500

    event-bus

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
@@ -11,12 +15,13 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
   posts[id] = { id, title };
 
+  await axios.post('http://localhost:4005/events', { type: 'PostCreated', data: posts[id] });
   res.status(201).send(posts[id]);
 });
 
